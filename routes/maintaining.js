162588786bc93221dddb257f6fae7e888f34bf18
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-11-21 17:25:27 +0300

    Implemented routing

--- routes/maintaining.js
+++ routes/maintaining.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title : 'LOL'});
+  res.render('maintaining');
 });
 
 module.exports = router;
