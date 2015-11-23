c48c528d573a0d39f8aed74026a1c4582d7d292d
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-11-24 00:38:24 +0300

    Fixed small issues, added some comments, css minfication config in WebStorm

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// here we are adding some routes (path to them in project's directory), if we want to use them as separate files
 var routes = require('./routes/index');
-var users = require('./routes/users');
-var maintaining = require('./routes/maintaining');
+//var users = require('./routes/users'); // this file should exist
 
 var app = express();
 
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,9 +24,9 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// and here we are stating our paths to routes in url access
 app.use('/', routes);
-app.use('/users', users);
-app.use('/maintaining', maintaining);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
