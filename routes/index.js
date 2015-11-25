548e6b3ccba060988c29f58b0e1844578a4f402f
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-11-25 10:59:31 +0300

    Routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,41 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Добро пожаловать!' });
+  res.render('pages/index', { title: 'Добро пожаловать!' });
 });
 
+/* STUDENTS SECTION */
+
+/* Student's survey */
 router.get('/survey', function(req, res, next) {
-   res.render('survey', { title: 'Страница опроса' })
+   res.render('pages/survey', { title: 'Страница опроса' });
+});
+
+/* Screen after finishing student's survey */
+router.get('/survey/finish', function(req, res, next) {
+    res.render('pages/survey/finish')
+});
+
+/* ADMIN SECTION */
+
+/* Root administrator's screen */
+router.get('/maintaining', function(req, res, next) {
+    res.render('pages/maintaining')
+});
+
+/* Here admin can add new surveys */
+router.get('/maintaining/create', function(req, res, next) {
+    res.render('pages/maintaining/create')
+});
+
+/* Here admin can schedule surveys */
+router.get('/maintaining/schedule', function(req, res, next) {
+    res.render('pages/maintaining/schedule')
+});
+
+/* Results of surveys */
+router.get('/maintaining/results', function(req, res, next) {
+    res.render('pages/maintaining/results')
 });
 
 module.exports = router;
