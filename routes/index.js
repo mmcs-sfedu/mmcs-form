e46c09da5fa8a4c1cb8e00eef282609369be768a
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-12-01 00:40:32 +0300

    Prepared PostgreSQL connection and route for DB testing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Добро пожаловать!' });
 });
 
+// TODO сделал временно для проверки подключения к БД
+// TODO удалить потом вместе с моделью temp_user
+var models = require('../models'); // including models class to access DB rows
+router.get('/db-test', function(req, res, next) {
+    models.User.findAll(
+        // { include: [ models.Task ]}
+    ).then(function(users) {
+           res.send(users);
+    });
+});
+
 module.exports = router;
