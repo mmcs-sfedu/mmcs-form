a3d49d95239c7e8aca21d773c48fcf5993e7a909
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-11-30 17:12:04 +0300

    Replaced get data logic to controller

--- controllers/survey.js
+++ controllers/survey.js
@@ -1,6 +1,7 @@
 module.exports =
 {
     getTestData : function() {
-        return [1, 2, 3, 4, 5];
+        var json = '{"feedbackForm": [{"title": "Насколько полезен с вашей точки зрения данный предмет?","type": "radio","options": ["5","4","3","2","1"]}]}';
+        return JSON.parse(json)['feedbackForm'];
     }
 };
