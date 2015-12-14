8bbca3fdc5e15a4d3831d35d02efb2fe81c182ef
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-12-14 16:11:05 +0300

    Seeds for disciplines and feedback forms.

--- seeders/20151214154015-default.js
+++ seeders/20151214154015-default.js
@@ -2,25 +2,36 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      var disciplines = [];
+      for (var i = 1; i <= 5; i++) {
+          disciplines.push({
+              teacher_id: i,
+              subject_id: i,
+              group_id: i,
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString()
+          });
+      }
+
+      var feedback_forms = [];
+      for (i = 1; i <= 5; i++) {
+          feedback_forms.push({
+              name: "Форма анкетирования " + i,
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString()
+          });
+      }
+
+      queryInterface.bulkInsert('disciplines', disciplines, {});
+      return queryInterface.bulkInsert('feedback_forms', feedback_forms, {});
+
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+      queryInterface.bulkDelete('feedback_forms', null, {});
+      return queryInterface.bulkDelete('disciplines', null, {});
+
   }
 };
