d9ec9e848a6ba3da470a7933361812e87769d57e
Andrei Koshmanskii <andrew_21_12@mail.ru>
2015-12-14 19:18:46 +0300

    All seeders for database testing are ready.

--- seeders/20151214185057-fstages.js
+++ seeders/20151214185057-fstages.js
@@ -1,26 +1,37 @@
 'use strict';
 
+var models = require('../models');
+
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      return models.feedback_form.findAll().then(function(feedback_forms) {
+          var feedback_stages = [];
+          for (var j = 0; j < feedback_forms.length; j++) {
+              var date = new Date();
+              for (var i = 1; i <= 3; i++) {
+                  var fromDate = new Date(date);
+                  var toDate = new Date(date);
+                  fromDate.setDate(date.getDate() + i * 5 - 5);
+                  toDate.setDate(date.getDate() + i * 5);
+                  feedback_stages.push({
+                      date_from: fromDate.toISOString(),
+                      date_to: toDate.toISOString(),
+                      feedback_form_id: feedback_forms[j].id,
+                      createdAt: new Date().toISOString(),
+                      updatedAt: new Date().toISOString()
+                  });
+              }
+          }
+
+          return queryInterface.bulkInsert('feedback_stages', feedback_stages, {});
+      });
+
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+      return queryInterface.bulkDelete('feedback_stages', null, {});
+
   }
 };
