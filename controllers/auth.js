09b7df32fc362e95bb4873e6a55a05c87c5d442e
Andrei Koshmanskii <andrew_21_12@mail.ru>
2016-02-04 00:26:42 +0300

    Reordered functions for authorization.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,138 +1,145 @@
-// TODO хранить в сессии, реализовать авторизацию с БРС!
-
+/* To get an access to DB. */
 var models = require('../models');
 
 
-var authControllerNamespace = {
-    /* STUDENT AUTHORIZATION BLOCK */
-
-    userIdOrHash : null,
-    userGroupId  : null,
-    userName     : null,
-
-    getStudentsAuthorization : function() {
-        return authControllerNamespace.userIdOrHash;
-    },
-
-
-
-    /* ADMIN AUTHORIZATION BLOCK */
-
-    /* Data about signed admin */
-    adminIdOrHash : null, // also stores administrator's auth status
-    adminName     : null,
-
-    /**
-     * Checks if admin has already authorized.
-     * @returns {String} Admin ID or null (if not authorized).
-     * */
-    isAdminAuthorized : function() {
-        return authControllerNamespace.adminIdOrHash;
-    }
-
-};
-
-
+/* Public methods. */
 module.exports =
 {
     /* STUDENT AUTHORIZATION BLOCK */
 
-    studentLogout : function() {
-        authControllerNamespace.userIdOrHash = null;
-        authControllerNamespace.userGroupId  = null;
-        authControllerNamespace.userName     = null;
-    },
+    studentAttemptLogin : studentAttemptLogin,
 
-    studentAttemptLogin : function(login, password, callback) {
-        if (login.length < 5) { // TODO для тестов
-            callback("Неправильный логин или пароль!");
-            return;
-        }
-        // TODO возвращает null в случ неудачи
+    getStudentsAuthorization : getStudentsAuthorization,
 
-        // TODO осторожно, хардкод! Всё должно приходить от БРС!
-        // TODO для pgsql нужно хардкодить рандомную функцию!
-        return models.discipline
-            .find({ order: "random()" })
-            .then(function(discipline) {
-                if (discipline == null) {
-                    callback("Ошибка базы данных - нет дисциплин!");
-                    return;
-                }
-                authControllerNamespace.userIdOrHash = 11;
-                authControllerNamespace.userGroupId  = discipline.group_id;
-                authControllerNamespace.userName     = "Вася Пупкин";
-                callback(null); })
-    },
+    getStudentsName : getStudentsName,
 
-    getStudentsAuthorization : function() {
-        return authControllerNamespace.userIdOrHash;
-    },
+    getStudentsGroupId : getStudentsGroupId,
 
-    getUserName : function() {
-        return authControllerNamespace.userName;
-    },
+    studentLogout : studentLogout,
 
-    getStudentsGroupId : function() {
-        return authControllerNamespace.userGroupId;
-    },
-
-
-    getStudentAuthChecker : function() {
-        return function(req, res, next) {
-            if (authControllerNamespace.getStudentsAuthorization()) {
-                next();
-            } else {
-                res.redirect("/");
-            }
-        }
-    },
+    getStudentAuthChecker : getStudentAuthChecker,
 
 
 
     /* ADMIN AUTHORIZATION BLOCK */
 
-    /**
-     * Implements logout for admin.
-     * */
-    adminLogout : function() {
-        authControllerNamespace.adminIdOrHash = null;
-        authControllerNamespace.adminName     = null;
-    },
+    adminAttemptLogin : adminAttemptLogin,
 
-    /**
-     * Makes an attempt to authorize user as admin.
-     * @param {String} login Administrator's login.
-     * @param {String} password Administrator's password.
-     * @param {Function} callback Login finished callback:
-       - pass null to it if login was successful;
-       - pass error string (with fail reason) to it if login was failed.
-     * */
-    adminAttemptLogin : function(login, password, callback) {
-        /* Getting stored config to validate user auth data */
-        var adminsConfig = require('../config/admins.json');
+    isAdminAuthorized : isAdminAuthorized,
 
-        /* MD5 password */
-        var crypto = require('crypto');
-        var passwordHash = crypto.createHash('md5').update(password).digest('hex');
+    getAdminName : getAdminName,
 
-        /* If there is no such login in config (or password mismatch) - throwing an error */
-        if (adminsConfig[login] == null || adminsConfig[login]['password'] != passwordHash) {
-            callback("Неправильный логин или пароль!");
-        } else {
-            /* Saving admin data and refreshing page */
-            authControllerNamespace.adminIdOrHash = adminsConfig[login];
-            authControllerNamespace.adminName = adminsConfig[login]['name'];
-            callback(null)
-        }
-    },
-
-    /* Returns admin ID if authorized */
-    isAdminAuthorized : authControllerNamespace.isAdminAuthorized,
-
-    /* Returns authorized admin name */
-    getAdminName : function() {
-        return authControllerNamespace.adminName;
-    }
-
+    adminLogout : adminLogout
 };
+
+
+
+/* STUDENT AUTHORIZATION BLOCK */
+
+var userIdOrHash, userGroupId, userName;
+
+function studentAttemptLogin (login, password, callback) {
+    if (login.length < 5) { // TODO для тестов
+        callback("Неправильный логин или пароль!");
+        return;
+    }
+    // TODO возвращает null в случ неудачи
+
+    // TODO осторожно, хардкод! Всё должно приходить от БРС!
+    // TODO для pgsql нужно хардкодить рандомную функцию!
+    return models.discipline
+        .find({ order: "random()" })
+        .then(function(discipline) {
+            if (discipline == null) {
+                callback("Ошибка базы данных - нет дисциплин!");
+                return;
+            }
+            userIdOrHash = 11;
+            userGroupId  = discipline.group_id;
+            userName     = "Вася Пупкин";
+            callback(null); })
+}
+
+function getStudentsAuthorization() {
+    return userIdOrHash;
+}
+
+function getStudentsName () {
+    return userName;
+}
+
+function getStudentsGroupId() {
+    return userGroupId;
+}
+
+function studentLogout () {
+    userIdOrHash = null;
+    userGroupId  = null;
+    userName     = null;
+}
+
+function getStudentAuthChecker() {
+    return function(req, res, next) {
+        if (getStudentsAuthorization()) {
+            next();
+        } else {
+            res.redirect("/");
+        }
+    }
+}
+
+
+
+/* ADMIN AUTHORIZATION BLOCK */
+
+/* Data about signed admin */
+var adminIdOrHash, // also stores administrator's auth status
+    adminName;
+
+/**
+ * Makes an attempt to authorize user as admin.
+ * @param {String} login Administrator's login.
+ * @param {String} password Administrator's password.
+ * @param {Function} callback Login finished callback:
+ - pass null to it if login was successful;
+ - pass error string (with fail reason) to it if login was failed.
+ * */
+function adminAttemptLogin(login, password, callback) {
+    /* Getting stored config to validate user auth data */
+    var adminsConfig = require('../config/admins.json');
+
+    /* MD5 password */
+    var crypto = require('crypto');
+    var passwordHash = crypto.createHash('md5').update(password).digest('hex');
+
+    /* If there is no such login in config (or password mismatch) - throwing an error */
+    if (adminsConfig[login] == null || adminsConfig[login]['password'] != passwordHash) {
+        callback("Неправильный логин или пароль!");
+    } else {
+        /* Saving admin data and refreshing page */
+        adminIdOrHash = adminsConfig[login];
+        adminName = adminsConfig[login]['name'];
+        callback(null)
+    }
+}
+
+/**
+ * Checks if admin has already authorized.
+ * @returns {String} Admin ID or null (if not authorized).
+ * */
+function isAdminAuthorized () {
+    return adminIdOrHash;
+}
+
+/* Returns authorized admin name */
+function getAdminName() {
+    return adminName;
+}
+
+/**
+ * Implements logout for admin.
+ * */
+function adminLogout() {
+    adminIdOrHash = null;
+    adminName     = null;
+}
